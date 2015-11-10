b926a18b0354a4a17cb14431b027db415780a247
UserK <balestrieri.gepp@gmail.com>
2015-11-10 19:25:51 +0100

    added singular values properties

---
+++ Tools Control/MaxSingVals/singularValuesProperties.m
@@ -0,0 +1,39 @@
+%% Max singular values
+clc 
+clear all 
+
+disp('Let us define a square matrix E');
+E = [ 1 2 3; 4+i 5+i 6+i; 7 0 9]
+disp('Eigenvalues of E:');
+disp(eig(E))
+% Es = transposed conjugate of E
+Estar = conj(E).';
+
+disp('Press X to continue');
+pause();
+%% Let's consider E*xE and ExE*
+clc
+disp('The eigenvalues of E*E are all real and positive');
+disp(eig(Estar*E))
+% conj(E).' is the same as E'*E. Much quicker
+%eig(E'*E)
+
+
+disp('The non zero eigenvalues of E*E are the same of EE*');
+disp(eig(E*Estar))
+%eig(E*E')
+
+disp('Press X to continue');
+pause();
+%% Singular Values
+clc
+% computes singular values of E with matlab function
+singularValuesE = svd(E)
+
+% singular values of E are the square root of the eig(E*E)
+disp('Square root of eig(E*E)');
+disp(sort(sqrt(eig(Estar*E)),'descend'))
+
+
+disp('Press X to continue');
+pause();
