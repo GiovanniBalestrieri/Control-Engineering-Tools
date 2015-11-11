f3bf8bf2202f1d020407fb78e3d9d343a8a60f42
UserK <balestrieri.gepp@gmail.com>
2015-11-11 18:40:29 +0100

    Unstable plant analysis nyquist

--- Tools Control/Nyquist/BasicNyquist.m
+++ Tools Control/Nyquist/BasicNyquist.m
@@ -30,8 +30,39 @@ figure(2)
 title('Open Loop');
 step(H)
 
-%% Nyquist Stable plant
+%% Nyquist UNStable plant
 
+P = zpk([1.3 -2],[-1 -2 -8 2],1);
+sysM = canon(P,'modal');
+%disp(sysC.a);
+disp('Realization of the following Transfer Function:');
+disp(P);
+disp('X');
+
+
+autoval = eig(sysM.a);
+for i= 1:size(sysM.a,1)
+    disp('Testing autoval:');
+    disp(autoval(i))
+    if (autoval(i)>=0)
+        disp('eig with non negative real part!!');
+    else
+        %disp('Ok');
+    end
+end
+
+figure(4)
+title('Open Loop');
+disp(sysM.a);
+step(P)
+
+% Closed loop
+
+CL = feedback(sysM,eye(size(sysM.d,1));
+figure(5)
+title('Closed Loop');
+step(CL)
+%%
 H = zpk([2 2 -3],[-2 -2 -2 -1],1)
 IPGH = 1 + H;
 nyquist(IPGH);
@@ -46,27 +77,3 @@ figure(2)
 title('Open Loop');
 step(H)
 
-%% Decomposizione valori singolari
-A = diag([1 2 3 4])
-%A'*A
-s = svd(A)
-
-% (x-1)(x-2)(x+3)
-u = [1  0  -7  6]
-A = compan(u)
-eig = eig(A)
-
-Sing = svd(A)
-max(Sing)
-% prova formula maxSingVal(A) = max_i sqrt(lambda-i*(A'*A)
-sqrt(eig(1)*(A'*A))
-sqrt(eig(2)*(A'*A))
-sqrt(eig(3)*(A'*A))
-
-
-% should be the same as Sing
-sqrt(eig(A'*A))
-
-Sing = svd(A)
-% massimo valore singolare
-max(Sing)
