c684f6b74f43a178bfed577f836d43a96a0ae6b6
UserK <balestrieri.gepp@gmail.com>
2016-01-26 18:08:16 +0100

    Solving non linear equations with iterations

--- Optimization/graph/nonLinSolver/solver.m
+++ Optimization/graph/nonLinSolver/solver.m
@@ -1,8 +1,10 @@
 % Limitations : It fsolve gives only one root. F must be continous
+% change the initial conditions and re run
 
 func = @fun;
-x0 = [0,0];
+x0 = [1,1];
 
 options = optimoptions('fsolve','Display'...
     ,'none','PlotFcns',@optimplotfirstorderopt);
-x = fsolve(func,x0,options)
+x = fsolve(func,x0,options);
+disp(x);
