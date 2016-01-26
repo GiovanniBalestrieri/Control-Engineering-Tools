11b1840b62a860c0d56bb767b41d2aa03ddf2e68
UserK <balestrieri.gepp@gmail.com>
2016-01-26 17:12:51 +0100

    Ex3

---
+++ Optimization/graph/graphEx3.m
@@ -0,0 +1,35 @@
+function graphF
+
+% Create figure
+figure1 = figure('Position',[1 400 1200 600]);
+colormap('gray');
+axis square;
+[X,Y] = meshgrid(-100:1:100);
+%Z=ex2(X,Y);
+Z=X.^3+Y.^2+3*X;
+
+
+% Create subplot
+subplot1 = subplot(1,2,1,'Parent',figure1);
+view([124 34]);
+grid('on');
+hold('all');
+
+% Create surface
+surf(X,Y,Z,'Parent',subplot1,'LineStyle','none');
+
+% Create contour
+contour(X,Y,Z,'Parent',subplot1);
+
+% Create subplot
+subplot2 = subplot(1,2,2,'Parent',figure1);
+view([234 34]);
+grid('on');
+hold('all');
+
+% Create surface
+surf(X,Y,Z,'Parent',subplot2,'LineStyle','none');
+
+% Create contour
+contour(X,Y,Z,'Parent',subplot2);
+end
