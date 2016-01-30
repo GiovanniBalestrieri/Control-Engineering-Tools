de68d82272360cdae275dc3d93cb17e1b8862d62
UserK <balestrieri.gepp@gmail.com>
2016-01-30 19:16:30 +0100

    Added graph function and expressions

--- Optimization/graph/graphEx4.m
+++ Optimization/graph/graphEx4.m
@@ -1,16 +1,22 @@
 function graphF
-
+clear all
+clc
+xmin = -1;
+xmax = 4;
+step = 0.1;
+numSamples = abs(xmax-xmin)*step^-1;
 % Create figure
 figure1 = figure('Position',[1 400 1200 600]);
 %colormap('gray');
 %axis square;
-[X,Y] = meshgrid(-1:0.1:3);
+[X,Y] = meshgrid(xmin:step:xmax);
 %Z=ex2(X,Y);
 %Z=X.^3+Y.^2+3*X;
 %Z = abs(X.^3);
 %Z=Y.^4 + X.^4 - 3*X*Y;
 %Z = 3*(X-Y).^3 - 4*(X-Y);
-Z = (3*(X).^2 +2 * X.*Y + 4*Y.^2 - 7*X -6*Y).^2;
+%Z = (1/2*(3*X.^2+2*X.*Y + 4*Y.^2) -7*X -6*Y).^2;
+
 
 % Create subplot
 subplot1 = subplot(1,2,1,'Parent',figure1);
