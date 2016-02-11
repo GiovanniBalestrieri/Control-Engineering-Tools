12fd1b7f2408a8b02774043629cf67eda145466d
UserK <balestrieri.gepp@gmail.com>
2016-02-11 23:27:03 +0100

    Ex piccialli Ottimizzazione

--- Optimization/osc1/lqrInf.m
+++ Optimization/osc1/lqrInf.m
@@ -1,7 +1,7 @@
 clear all;
 clc;
 
-A=[ 0 2 ; 1 0];
+A=[ 0 -1 ; 2 -2];
 B = [0;1];
 C = [1 1];
 D = [0];
@@ -9,8 +9,8 @@ D = [0];
 sys = ss(A,B,C,D);
 %Calcolo retroazione dallo stato
 disp('Uso la funzione lqr per calcolare il guadagno K (la retroazione dallo stato u=Kx)')
-Q=[3 1;1 2];
-R=eye(1)
+Q=1/2*[12 -3;-3 1];
+R=eye(1)/2
 K = lqr(sys.a,sys.b,Q,R);
 disp('Matrice di guadagno K: [comando K = lqr(A,B,Q,R)]');
 disp(K);
