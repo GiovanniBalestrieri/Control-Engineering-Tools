02aee35aac501cb824e9be21b726999370dee5bd
UserK <balestrieri.gepp@gmail.com>
2016-02-15 21:56:25 +0100

    Added exam ottimizzazione

--- Optimization/graph/graphF.m
+++ Optimization/graph/graphF.m
@@ -8,7 +8,8 @@ axis square;
 %Z=ex2(X,Y);
 %Z=1/3*(X-2).^3 -X*Y.^2;
 %Z = (1/2*(3*X.^2+2*X.*Y+2*Y.^2)-7*X -6*Y).^2
-Z = 1/2*(3*X.^2+2*X.*Y+1/3*Y.^2)-3*X +1*Y
+%Z = 1/2*(3*X.^2+2*X.*Y+1/3*Y.^2)-3*X +1*Y
+Z = 3*(X-Y).^3 -4*(X-Y)
 
 % Create subplot
 subplot1 = subplot(1,2,1,'Parent',figure1);
