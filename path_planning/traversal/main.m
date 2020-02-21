7129886df382a9ff0d80ee8e1973d00d48c47120
Giovanni <giovanni.balestrieri@inria.fr>
2020-02-21 12:42:17 +0100

    going for the path planning task

--- path_planning/traversal/main.m
+++ path_planning/traversal/main.m
@@ -3,14 +3,14 @@ clc
 ###################################################
 # Utility variables
 ###################################################
-plotting = true
+plotting = false
 
 ###################################################
 # Path Planning
 ###################################################
 
 load maps
-map = [ 0, 0, 1; 1, 1 ,1 ;0 , 0, 0];
+map = [ 0, 2; 0, 0];
 
 
 
@@ -24,6 +24,33 @@ previous = NodeZ;
 
 # Create graph from grid
 g = populate_graph_from_ogrid(map);
+
+## Path Planning
+cost_fcn = 0;
+cells_to_visit = {};
+path = {}; # stores the result
+for i=1:size(g.nodes,2)
+  cells_to_visit{i} = g.nodes{i};
+endfor
+
+# Start from Source
+path{1} = g.start;
+node_cur = g.start;
+# set it to visited in the nodes list struct of the graph
+g.nodes{g.start.id}.visited = 1
+# remove start node form to_visit
+index = vindex(cells_to_visit,node_cur)
+if index > 0
+  cells_to_visit{index} = []
+endif
+# Go to adj of cur_node
+
+for x=1:size(g.adjacencies{node_cur.id},2)
+  if g.adjacencies{x}.visited == 0 # not visited yet
+    
+  endif
+endfor
+
 
 disp("Numer of obstacles: ")
 disp(g.obstacles)
